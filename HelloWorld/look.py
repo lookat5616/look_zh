0150f79f1856c4a80bae45786de0969a11e847b8
zhangfu <zhangfu131484@126.com>
2016-06-15 17:25:26 +0800

    test

---
+++ HelloWorld/look.py
@@ -0,0 +1,35 @@
+#coding:utf-8
+#coding
+'''
+Created on 2016年5月9日
+
+@author: look
+'''
+class Animal(object):
+    def run(self):
+        print "Animal is running..."
+        
+class Dog(Animal):
+    def run(self):
+        print 'Dog is running...'
+    def eat(self):
+        print 'Eating meat...'
+
+class Cat(Animal):
+    def run(self):
+        print 'Cat is running...'
+
+dog = Dog()
+dog.run()
+print dog.__class__
+print type(dog)
+
+cat = Cat()
+cat.run()
+print cat.__class__
+print type(cat)
+print type(123)
+
+
+
+
